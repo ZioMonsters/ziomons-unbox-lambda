922841813467750b6413a4fde1c774081fa39ba8
Ng2k <nicola.ng2k@gmail.com>
2018-09-07 20:16:38 +0200

    put sulla table del nouvo mostro e invoke lambda immagini

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
-const AWS = require("aws-sdk")
-// instanzi il document client
-const TableName = `cryptomon-monsters-${process.env.NODE_ENV}`
-const EventsTable = `cryptomon-events-${process.env.NODE_ENV}`
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient({ region: 'eu-west-3' });
+const lambda = new AWS.Lambda.DocumentClient({ region: 'eu-west-3' });
+
+const TableName = `cryptomon-monsters-${process.env.NODE_ENV}`;
+const EventsTable = `cryptomon-events-${process.env.NODE_ENV}`;
 
 exports.handler = (event, context, callback) => {
+  /*
+    qua ci saranno i valori presi dall'evento che poi scrivo su dynamo
+  */
+  const eventObj = {
+    tokenId,
+    to,
+    atk /*recupero con web3*/,
+    def /*recupero con web3*/,
+    spd /*recupero con web3*/
+  };
+
   return Promise.all([
-    // promise che mette sulla table di dynamo il mostro con le sue caratteristiche (PartitionKey = monsterId, SortKey = user)
-    // promise che fa partire la lambda images
-    // promise che segni sulla table events l'evento come processato
+    dynamo.batchWrite({
+      TableName,
+      Item: {
+        monsterId: eventObj.tokenId,
+        user: eventObj.to,
+        atk: eventObj.atk,
+        def: eventObj.def,
+        spd: eventObj.spd
+      }
+    }).promise(),
+    lambda.invoke({
+      FunctionName: 'cryptomon-images-lambda',
+      Payload: JSON.stringify({ /*id del mostro*/ })
+    }).promise(),
+  // promise che mette sulla table di dynamo il mostro con le sue caratteristiche (PartitionKey = monsterId, SortKey = user)
+  // promise che fa partire la lambda images
+  // promise che segni sulla table events l'evento come processato
   ])
     .then(() => callback(null, event))
-}
+};
