8f5fb07a033cf368a270ae121277002f04e59ba8
Ng2k <nicola.ng2k@gmail.com>
2018-09-07 21:19:33 +0200

    Attribute process settata true di un evento processato

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ exports.handler = (event, context, callback) => {
   /*
     qua ci saranno i valori presi dall'evento che poi scrivo su dynamo
   */
+  const eventId = 1;
   const eventObj = {
     tokenId,
     to,
@@ -32,6 +33,14 @@ exports.handler = (event, context, callback) => {
       FunctionName: 'cryptomon-images-lambda',
       Payload: JSON.stringify({ /*id del mostro*/ })
     }).promise(),
+    dynamo.put({
+      TableName: EventsTable,
+      Item: {
+        /*attributes dell'evento (non so come e' salvato)*/
+        eventId,
+        processed: true
+      }
+    }).promise()
   // promise che mette sulla table di dynamo il mostro con le sue caratteristiche (PartitionKey = monsterId, SortKey = user)
   // promise che fa partire la lambda images
   // promise che segni sulla table events l'evento come processato
